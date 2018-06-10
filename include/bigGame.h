d462a8e9b86e341d40853c8b9936e8a4e3e25173
BarbaraCortes <barbaracortes.98@hotmail.com>
2018-06-10 16:14:35 -0300

    comentei os .h

--- include/bigGame.h
+++ include/bigGame.h
@@ -9,18 +9,32 @@
 class BigGame {
 
 private:
+	/* Estabelece qual será o game onde o proximo jogador devera jogar */
 	std::pair<int, int> setGame(int i, int j);
+	
+	/* Determina se uma posicao eh valida, isto eh, esta desocupada e pertence ao game correto */
 	bool validPos(std::pair<int, int> g, std::pair<int, int> p);
 	
 public:
 	Game tab[3][3];
 	std::pair<int, int> curGame;
 
+	/* Construtor de um BigGame */
 	BigGame();
+
+	/* Checa se um jogador ganhou o BigGame */
 	int checkWin();
+
+	/* Checa se não ha mais posicoes para serem marcadas no BigGame */
 	bool checkFull();
+
+	/* Marca no tabuleiro a posicao onde o jogador decidiu jogar */
 	bool markPos(std::pair<int, int> g, std::pair<int, int> p, int player);
+
+	/* Retorna a string de 81 caracteres */
 	std::string completeGame();
+
+	/* Transforma o BigGame em uma string */
 	std::string toString();
 };
 
--- include/game.h
+++ include/game.h
@@ -6,11 +6,21 @@
 
 class Game {
 public:
-	int mat[3][3];
+	int mat[3][3]; 
+
+	/* Construtor de um game */
 	Game();
+	
+	/* Determina se o game foi ganho por algum jogador */
 	int checkWin();
+	
+	/* Determina se não ha mais posicoes para serem marcadas naquele game */
 	bool checkFull();
+	
+	/* Determina se uma posicao eh valida, isto eh, se esta livre */
 	bool validPos(int i, int j);
+	
+	/* Transforma o game em uma string */
 	std::string toString();
 };
 
